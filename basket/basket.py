9962c452039d623176a8b7d12364d4742816fb68
jetigenov <jetigenov@gmail.com>
2021-04-23 20:10:44 +0600

    fix

--- basket/basket.py
+++ basket/basket.py
@@ -8,9 +8,20 @@ class Basket():
         self.basket = basket
 
     def add(self, product, qty):
-        product_id = product.id
-        if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'quantity': int(qty)}
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
 
         self.session.modified = True
 
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
