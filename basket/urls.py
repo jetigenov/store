f1185110d5b12be671ec497335bc3fc19b7f382d
jetigenov <jetigenov@gmail.com>
2021-04-24 18:01:37 +0600

    added delete update session

--- basket/urls.py
+++ basket/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'basket'
@@ -6,4 +7,6 @@ app_name = 'basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
     path('add/', views.basket_add, name='basket_add'),
+    path('delete/', views.basket_delete, name='basket_delete'),
+    path('update/', views.basket_update, name='basket_update'),
 ]
