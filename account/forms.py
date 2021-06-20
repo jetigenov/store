481bb99df74b88de18ed14dda8552365a15e6a99
jetigenov <jetigenov@gmail.com>
2021-06-20 18:53:24 +0600

    login

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import User
+
+
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
 
 
 class RegistrationForm(forms.ModelForm):
