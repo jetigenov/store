294fa9d6f5dd69d8f6bf87b29a0cd6bfc68e64d2
jetigenov <jetigenov@gmail.com>
2021-06-25 13:47:27 +0600

    deleete

--- account/views.py
+++ account/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from .models import User
 from .token import account_activation_token
 from django.contrib.auth.decorators import login_required
@@ -58,3 +58,25 @@ def account_activate(request, uidb64, token):
         return redirect('account:dashboard')
     else:
         return render(request, 'account/registration/activation_invalid.html')
+
+
+@login_required()
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+
+    return render(request, 'account/user/edit_details.html', {'user_form': user_form})
+
+
+@login_required()
+def delete_user(request):
+    user = User.objects.get(user_name=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirmation')
