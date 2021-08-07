1121088ccdc301e1c2e7f743f63d28530a6d3d40
jetigenov <jetigenov@gmail.com>
2021-08-07 20:56:41 +0600

    payment complete function

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from account.models import Address
 from basket.basket import Basket
+from orders.models import OrderItem, Order
 from .models import DeliveryOptions
 
 
@@ -70,6 +71,38 @@ def payment_complete(request):
     PPClient = PayPalClient()
 
     body = json.loads(request.body)
-    data = body['orderID']
+    data = body["orderID"]
     user_id = request.user.id
-    requestorder = OrderGetRequest(data)
+
+    requestorder = OrdersGetRequest(data)
+    response = PPClient.client.execute(requestorder)
+
+    total_paid = response.result.purchase_units[0].amount.value
+
+    basket = Basket(request)
+    order = Order.objects.create(
+        user_id=user_id,
+        full_name=response.result.purchase_units[0].shipping.name.full_name,
+        email=response.result.payer.email_address,
+        address1=response.result.purchase_units[0].shipping.address.address_line_1,
+        address2=response.result.purchase_units[0].shipping.address.admin_area_2,
+        postal_code=response.result.purchase_units[0].shipping.address.postal_code,
+        country_code=response.result.purchase_units[0].shipping.address.country_code,
+        total_paid=response.result.purchase_units[0].amount.value,
+        order_key=response.result.id,
+        payment_option="paypal",
+        billing_status=True,
+    )
+    order_id = order.pk
+
+    for item in basket:
+        OrderItem.objects.create(order_id=order_id, product=item["product"], price=item["price"], quantity=item["qty"])
+
+    return JsonResponse("Payment completed!", safe=False)
+
+
+@login_required
+def payment_successful(request):
+    basket = Basket(request)
+    basket.clear()
+    return render(request, "checkout/payment_successful.html", {})
